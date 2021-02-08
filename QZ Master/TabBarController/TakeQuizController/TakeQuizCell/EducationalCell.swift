76cd077238a4f7f32e66e7a91e8674da5501df30
Alexandru Vorojbit <a.vorojbit@gmail.com>
2021-02-08 13:50:58 +0200

    commit

--- QZ Master/TabBarController/TakeQuizController/TakeQuizCell/EducationalCell.swift
+++ QZ Master/TabBarController/TakeQuizController/TakeQuizCell/EducationalCell.swift
@@ -1,90 +1,90 @@
+////
+////  EducationalCell.swift
+////  QZ Master
+////
+////  Created by Alexandru Vorojbit on 12/21/20.
+////
 //
-//  EducationalCell.swift
-//  QZ Master
+//import UIKit
 //
-//  Created by Alexandru Vorojbit on 12/21/20.
+//class EducationalCell: UICollectionViewCell {
+//    
+//    var educational: SectionItems? {
+//        didSet {
+//            nameLabel.text = educational?.sectionName
+//            educationalImageView.loadImageUsingCacheWithUrlString(urlString: educational?.sectionImage ?? "")
+//        }
+//    }
+//    
+//    // UI components
+//    lazy var educationalImageView: UIImageView = {
+//        let img = UIImageView()
+//        img.clipsToBounds = true
+//        img.contentMode = .scaleAspectFill
+//        img.translatesAutoresizingMaskIntoConstraints = false
+//        return img
+//    }()
+//    
+//    lazy var nameLabel: UILabel = {
+//        let label = UILabel()
+//        label.font = UIFont.preferredFont(forTextStyle: .title1).bold()
+//        label.adjustsFontForContentSizeCategory = true
+//        label.textColor = .label
+//        label.textAlignment = .left
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
+//    }()
+//    
+//    
+//    // Initialiser
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        
+//        backgroundColor = UIColor.systemGray6
+//        layer.cornerRadius = 16
+//        clipsToBounds = true
+//        
+//        setupViews()
+//    }
+//    
+//    
+//    /// Sets up the UI components for this cell and adds them to the contentview
+//    private func setupViews() {
+////        addSubview(educationalImageView)
+////        addSubview(nameLabel)
+////
+////        NSLayoutConstraint.activate([
+////            educationalImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+////            educationalImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+////            educationalImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+////            educationalImageView.heightAnchor.constraint(equalToConstant: 350),
+////
+////            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
+////            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+////            nameLabel.bottomAnchor.constraint(equalTo: educationalImageView.topAnchor, constant: -24)
+////        ])
+//        
+//        let stackView = VerticalStackView(arrangedSubviews: [nameLabel, educationalImageView], spacing: 12)
+//        addSubview(stackView)
+//        stackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 24, left: 24, bottom: 24, right: 24))
+//    }
+//    
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 //
-
-import UIKit
-
-class EducationalCell: UICollectionViewCell {
-    
-    var educational: SectionItems? {
-        didSet {
-            nameLabel.text = educational?.sectionName
-            educationalImageView.loadImageUsingCacheWithUrlString(urlString: educational?.image ?? "")
-        }
-    }
-    
-    // UI components
-    lazy var educationalImageView: UIImageView = {
-        let img = UIImageView()
-        img.clipsToBounds = true
-        img.contentMode = .scaleAspectFill
-        img.translatesAutoresizingMaskIntoConstraints = false
-        return img
-    }()
-    
-    lazy var nameLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .title1).bold()
-        label.adjustsFontForContentSizeCategory = true
-        label.textColor = .label
-        label.textAlignment = .left
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    
-    // Initialiser
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        backgroundColor = UIColor.systemGray6
-        layer.cornerRadius = 16
-        clipsToBounds = true
-        
-        setupViews()
-    }
-    
-    
-    /// Sets up the UI components for this cell and adds them to the contentview
-    private func setupViews() {
-//        addSubview(educationalImageView)
-//        addSubview(nameLabel)
-//
-//        NSLayoutConstraint.activate([
-//            educationalImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-//            educationalImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-//            educationalImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-//            educationalImageView.heightAnchor.constraint(equalToConstant: 350),
-//
-//            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
-//            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
-//            nameLabel.bottomAnchor.constraint(equalTo: educationalImageView.topAnchor, constant: -24)
-//        ])
-        
-        let stackView = VerticalStackView(arrangedSubviews: [nameLabel, educationalImageView], spacing: 12)
-        addSubview(stackView)
-        stackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 24, left: 24, bottom: 24, right: 24))
-    }
-    
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    
-    override var isHighlighted: Bool {
-        
-        didSet {
-            var transform = CGAffineTransform.identity
-            if isHighlighted {
-               transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
-            }
-            UIView.animate(withDuration: 0.9, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: .curveEaseOut, animations: {
-                self.transform = transform
-            })
-        }
-    }
-}
+//    
+//    override var isHighlighted: Bool {
+//        
+//        didSet {
+//            var transform = CGAffineTransform.identity
+//            if isHighlighted {
+//               transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+//            }
+//            UIView.animate(withDuration: 0.9, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: .curveEaseOut, animations: {
+//                self.transform = transform
+//            })
+//        }
+//    }
+//}
--- QZ Master/TabBarController/TakeQuizController/TakeQuizCell/MultipleQuizCell.swift
+++ QZ Master/TabBarController/TakeQuizController/TakeQuizCell/MultipleQuizCell.swift
@@ -1,97 +1,107 @@
-////
-////  BusinessCell.swift
-////  QZ Master
-////
-////  Created by Alexandru Vorojbit on 12/21/20.
-////
 //
-//import UIKit
+//  BusinessCell.swift
+//  QZ Master
 //
-//class MultipleQuizCell: UICollectionViewCell {
-//    
-//    var quizes: Quizes? {
-//        didSet {
-//            nameLabel.text = quizes?.name
-//            imageView.loadImageUsingCacheWithUrlString(urlString: quizes?.image ?? "")
-//        }
-//    }
-//    
-//    // UI components
-//    let imageView: UIImageView = {
-//        let img = UIImageView()
-//        img.clipsToBounds = true
-//        img.contentMode = .scaleAspectFit
-//        img.layer.cornerRadius = 16
-//        img.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-//        img.translatesAutoresizingMaskIntoConstraints = false
-//        return img
-//    }()
-//    
-//    let nameLabel: UILabel = {
-//        let label = UILabel()
-//        label.font = UIFont.preferredFont(forTextStyle: .title1).bold()
-//        label.adjustsFontForContentSizeCategory = true
-//        label.textColor = .label
-//        label.textAlignment = .left
-////        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//    
-//    let playButton: UIButton = {
-//        let button = UIButton(type: .custom)
-//        button.setImage(UIImage(systemName: "icloud.and.arrow.down"), for: .normal)
-//        button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        return button
-//    }()
-//    
-//    let separatorView: UIView = {
-//        let view = UIView()
-//        view.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
-//        return view
-//    }()
-//    
-//    
-//    // Initializer
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        backgroundColor = UIColor.systemGray6
-//        layer.cornerRadius = 16
-//        clipsToBounds = true
-//        setupViews()
-//    }
-//    
-//    // Sets up the UI components for this cell and adds them to the contentview
-//    private func setupViews() {
-//        
-//        let stackView = UIStackView(arrangedSubviews: [imageView, nameLabel, playButton])
-//        stackView.spacing = 16
-//        stackView.alignment = .center
-//        
-//        addSubview(stackView)
-//        stackView.fillSuperview()
-//        
-//        addSubview(separatorView)
-//        separatorView.anchor(top: nil, leading: nameLabel.leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: -8, right: 0), size: .init(width: 0, height: 0.5))
-//        
-//    }
-//    
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override var isHighlighted: Bool {
-//        
-//        didSet {
-//            var transform = CGAffineTransform.identity
-//            if isHighlighted {
-//               transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
-//            }
-//            UIView.animate(withDuration: 0.9, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: .curveEaseOut, animations: {
-//                self.transform = transform
-//            })
-//        }
-//    }
+//  Created by Alexandru Vorojbit on 12/21/20.
 //
-//}
+
+import UIKit
+
+class MultipleQuizCell: UICollectionViewCell {
+    
+    var quizes: SectionItems? {
+        didSet {
+            nameLabel.text = quizes?.quizName
+            imageView.loadImageUsingCacheWithUrlString(urlString: quizes?.quizImage ?? "")
+        }
+    }
+    
+    // UI components
+    let imageView: UIImageView = {
+        let img = UIImageView()
+        img.clipsToBounds = true
+        img.contentMode = .scaleAspectFit
+        img.layer.cornerRadius = 16
+        img.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title1).bold()
+        label.adjustsFontForContentSizeCategory = true
+        label.textColor = .label
+        label.textAlignment = .left
+//        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "icloud.and.arrow.down"), for: .normal)
+        button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
+        return view
+    }()
+    
+    
+    // Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.systemGray6
+        layer.cornerRadius = 16
+        clipsToBounds = true
+        
+        setupViews()
+       
+        imageView.backgroundColor = .purple
+        imageView.constrainWidth(constant: 64)
+        imageView.constrainHeight(constant: 64)
+        
+        playButton.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        playButton.constrainWidth(constant: 44)
+        playButton.constrainHeight(constant: 44)
+    }
+    
+    // Sets up the UI components for this cell and adds them to the contentview
+    private func setupViews() {
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, nameLabel, playButton])
+        stackView.spacing = 16
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.fillSuperview()
+        
+        addSubview(separatorView)
+        separatorView.anchor(top: nil, leading: nameLabel.leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: -8, right: 0), size: .init(width: 0, height: 0.5))
+        
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isHighlighted: Bool {
+        
+        didSet {
+            var transform = CGAffineTransform.identity
+            if isHighlighted {
+               transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            }
+            UIView.animate(withDuration: 0.9, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: .curveEaseOut, animations: {
+                self.transform = transform
+            })
+        }
+    }
+
+}
